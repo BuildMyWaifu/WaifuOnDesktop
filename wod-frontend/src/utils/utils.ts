1b786204b370b24d4cb115195eeb6adb54f88618
Kulimi <kulimi@kulimi.tw>
2024-12-06 14:23:07 +0800

    feat: 添加了登入後，儲存登入紀錄，並且自動登入

--- wod-frontend/src/utils/utils.ts
+++ wod-frontend/src/utils/utils.ts
@@ -1,5 +1,22 @@
 import crypto from 'crypto-js'
 
+export function getCookie(cname: string) {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
 export function copy (data: object) {
   return JSON.parse(JSON.stringify(data))
 }
