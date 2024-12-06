628a29fd8a29c519e8143c3d969c7d7aba237cad
Kulimi <kulimi@kulimi.tw>
2024-12-06 14:48:14 +0800

    feat: 修復了在 main.vue 中沒有使用 v-main

--- wod-frontend/src/components/CompanionPreview.vue
+++ wod-frontend/src/components/CompanionPreview.vue
@@ -1,15 +1,14 @@
 <template>
 
 
-  <v-container id='drawer' class="d-flex flex-column align-center pt-10" style="height: 100%;">
-
-    <!-- <v-sheet class="mb-4" width="60%" color="grey" style="aspect-ratio: 1; border: 4px solid gray; border-radius: 10%;">
-      <v-img
-        src="https://upload.wikimedia.org/wikipedia/zh/thumb/f/fc/%E5%8E%9F%E7%A5%9E_%E5%9C%8B%E9%9A%9B%E7%89%88.jpeg/220px-%E5%8E%9F%E7%A5%9E_%E5%9C%8B%E9%9A%9B%E7%89%88.jpeg"
-        alt="image" max-width="100%" max-height="100%" contain style="border-radius: 10%;"></v-img>
-    </v-sheet> -->
-    <v-row justify="center">
-      <v-col class="text-center">
+  <v-container>
+    <v-row>
+      <v-col cols="6">
+        <v-container>
+          <Live2dComponent ></Live2dComponent>
+        </v-container>
+      </v-col>
+      <v-col cols="6" class="text-center">
         <v-card-title class="text-h5">
           {{ companion.profile.name }}
         </v-card-title>
@@ -18,23 +17,23 @@
         </v-card-text>
 
 
-        <v-card>
+        <v-container>
           <v-card-title class="text-h6">
             性格
           </v-card-title>
           <v-card-text class="text-h7" style="border: 1px solid #ccc; border-radius:10px; padding: 16px;">
             {{ companion.prompt.character }}
           </v-card-text>
-        </v-card>
+        </v-container>
 
-        <v-card>
-          <v-card-title class="text-hˊ">
+        <v-container>
+          <v-card-title class="text-h">
             背景設定
           </v-card-title>
           <v-card-text class="text-h7" style="border: 1px solid #ccc; border-radius:10px; padding: 16px;">
             {{ companion.prompt.backstory }}
           </v-card-text>
-        </v-card>
+        </v-container>
 
         <v-dialog max-width="500">
           <template v-slot:activator="{ props: activatorProps }">
@@ -58,6 +57,7 @@
         </v-dialog>
 
       </v-col>
+
     </v-row>
   </v-container>
 
@@ -65,7 +65,8 @@
 
 </template>
 <script lang="ts" setup>
-import { useAppStore } from '@/stores/app';
+  import { useAppStore } from '@/stores/app';
+import Live2dComponent from './Live2dComponent.vue';
 import { copy } from '@/utils/utils';
 import { computed, ref } from 'vue';
 
