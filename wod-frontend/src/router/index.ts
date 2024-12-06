42ff799b01a5f3de981c04e6d194274ef0794021
Kulimi <kulimi@kulimi.tw>
2024-12-06 14:23:07 +0800

    feat: 添加了登入後，儲存登入紀錄，並且自動登入

--- wod-frontend/src/router/index.ts
+++ wod-frontend/src/router/index.ts
@@ -5,74 +5,97 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { useAppStore } from "@/stores/app";
+import { fetchApi } from "@/utils/api";
+import { createRouter, createWebHistory } from "vue-router/auto";
+import { getCookie } from "@/utils/utils";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/pages/index.vue'),
+    path: "/",
+    component: () => import("@/pages/index.vue"),
     meta: { requiresAuth: false },
   },
   {
-    path: '/app',
-    name: 'Home',
-    component: () => import('@/pages/Main.vue'),
+    path: "/app",
+    name: "Home",
+    component: () => import("@/pages/Main.vue"),
     meta: { requiresAuth: true },
   },
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     meta: { requiresAuth: false },
-    component: () => import('@/pages/Login.vue'),
+    component: () => import("@/pages/Login.vue"),
   },
   {
-    path: '/signup',
-    name: 'SignUp',
+    path: "/signup",
+    name: "SignUp",
     meta: { requiresAuth: false },
-    component: () => import('@/pages/SignUp.vue'),
+    component: () => import("@/pages/SignUp.vue"),
   },
   {
-    path: '/test',
-    name: 'Test',
+    path: "/test",
+    name: "Test",
     meta: { requiresAuth: false },
-    component: () => import('@/pages/TestPage.vue'),
+    component: () => import("@/pages/TestPage.vue"),
   },
   {
-    path: '/:pathMatch(.*)*',
-    name: 'Error',
+    path: "/:pathMatch(.*)*",
+    name: "Error",
     meta: { requiresAuth: false },
-    component: () => import('@/pages/ErrorPage.vue'),
+    component: () => import("@/pages/ErrorPage.vue"),
   },
   {
-    path: '/liveChat',
-    name: 'LiveChat',
+    path: "/liveChat",
+    name: "LiveChat",
     meta: { requiresAuth: false },
-    component: () => import('@/pages/LiveChatPage.vue'), // Ensure the casing matches
-  }
-]
+    component: () => import("@/pages/LiveChatPage.vue"), // Ensure the casing matches
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
+  if (err?.message?.includes?.("Failed to fetch dynamically imported module")) {
+    if (!localStorage.getItem("vuetify:dynamic-reload")) {
+      console.log("Reloading page to fix dynamic import error");
+      localStorage.setItem("vuetify:dynamic-reload", "true");
+      location.assign(to.fullPath);
     } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
+      console.error("Dynamic import error, reloading page did not fix it", err);
     }
   } else {
-    console.error(err)
+    console.error(err);
   }
-})
+});
 
 router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
-})
+  localStorage.removeItem("vuetify:dynamic-reload");
+});
 
-export default router
+router.beforeEach(async function (to) {
+  const store = useAppStore();
+  if (to.meta.requiresAuth && !store.user) {
+    const res = await fetchApi("/me");
+    if (res.status == "success") {
+      store.login(res.data);
+    } else {
+      if (getCookie("token").length > 0) {
+        alert("登入已過期！請重新登入");
+      } else {
+        alert("您尚未登入！此頁面登入後方可閱覽");
+      }
+      return {
+        path: "/login",
+        query: { redirect: to.fullPath },
+      };
+    }
+  }
+});
+
+export default router;
