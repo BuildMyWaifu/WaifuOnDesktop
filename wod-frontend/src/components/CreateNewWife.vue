cf0481fe21724b197ec87a23dbeadbfca69f2d18
Kulimi <kulimi@kulimi.tw>
2024-12-06 23:44:34 +0800

    feat(CreateNewWife): 修復無法選擇模板的錯誤，並且變更樣式

--- wod-frontend/src/components/CreateNewWife.vue
+++ wod-frontend/src/components/CreateNewWife.vue
@@ -1,50 +1,40 @@
+<!-- eslint-disable vue/valid-v-slot -->
 <template>
-    <v-dialog v-model="isDialogOpen" width="500">
-        <template v-slot:activator="{props}">
-        <v-btn v-bind="props"
-        variant="flat">
-            <template v-slot:prepend>
-            <v-icon icon="mdi-plus"></v-icon>
-            </template>
-            新增老婆
-        </v-btn>
-        </template>
-        
-        <v-container>
-            <v-stepper v-model="currentStep" :items="steps" hide-actions>
-                <template v-slot:item.1>
-                    <v-row dense>
-                        <v-col cols="12" md="4" v-for="(wife, index) in wives" :key="index">
-                            <v-card variant="plain" @click="selectCard(index)"
-                                :class="{ 'selected-card': selectedCard === index }">
-                                <v-card-title>{{ wife.profile.name }}</v-card-title>
-                                <v-card-text>{{ wife.profile.description }}</v-card-text>
-                            </v-card>
-                        </v-col>
-                    </v-row>
-                    <v-card-actions>
-                        <v-btn color="primary" @click="prevStep" :disabled="currentStep === 1">
-                            上一步
-                        </v-btn>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary" @click="nextStep" :disabled="selectedCard === null">
-                            下一步
-                        </v-btn>
-                    </v-card-actions>
+    <v-dialog v-model="isDialogOpen" @update:model-value="() => {
+        currentStep = 1
+    }">
+        <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" variant="flat">
+                <template v-slot:prepend>
+                    <v-icon icon="mdi-plus"></v-icon>
                 </template>
-
+                新增老婆
+            </v-btn>
+        </template>
+        <v-container>
+            <v-stepper v-model="currentStep" :items="steps" prev-text="上一步"
+                :next-text="currentStep == 2 ? '完成' : '下一步'">
+                <template v-slot:item.1>
+                    <v-card-subtitle>從範本建立，或是從零開始打造</v-card-subtitle>
+                    <div class="d-flex">
+                        <v-list width="256px" lines="three">
+                            <v-list-item v-for="companion, i in wives" :key="i" :title="companion.profile.name"
+                                :subtitle="companion.profile.description" @click="selectBase(i)"
+                                :active="baseCompanionIndex == i">
+                            </v-list-item>
+                        </v-list>
+                        <v-divider vertical></v-divider>
+                        <CompanionPreview readonly style="height: 60vh;width: 60vh"
+                            :companion="wives[baseCompanionIndex]">
+                        </CompanionPreview>
+                    </div>
+                </template>
                 <template v-slot:item.2>
-                    <CompanionEdit v-model="companion"></CompanionEdit>
-                    <v-divider></v-divider>
-                    <v-card-actions>
-                        <v-btn color="primary" @click="prevStep" :disabled="currentStep === 1">
-                            上一步
-                        </v-btn>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary" @click="CloseDialog">
-                            完成
-                        </v-btn>
-                    </v-card-actions>
+                    <CompanionEdit v-if="companion != undefined" v-model="companion"></CompanionEdit>
+                    <v-card-text v-else>
+                        不合法的選擇，請回上一步
+                        <div class="text-caption text-grey">如果見到此訊息，請視作錯誤回報</div>
+                    </v-card-text>
                 </template>
             </v-stepper>
         </v-container>
@@ -53,128 +43,100 @@
 
 <script lang="ts" setup>
     import CompanionEdit from './CompanionEdit.vue';
+    import CompanionPreview from '@/components/CompanionPreview.vue'
     import { Companion } from '@/utils/model';
+    import { copy } from '@/utils/utils'
+    import { ref } from 'vue';
 
     const isDialogOpen = ref(false)
     const steps = ['選擇老婆', '設定']
     const currentStep = ref(1)
-    const selectedCard = ref() // 儲存選擇的卡片索引
-    const companion = ref<Companion>({
-        _id: '',
-        userId: '',
-        profile: {
-            name: '',
-            description: '',
-        },
-        prompt: {
-            character: '',
-            backstory: '',
-        },
-    });
-
     const wives = [
-    {
-        _id: "咪醬",
-        userId: '',
-        profile: {
-        name: "咪醬",
-        description: "可愛又元氣滿滿的貓娘女僕，喜歡撒嬌和主人喵～！",
-        },
-        prompt: {
-        character: "元氣可愛的貓娘女僕",
-        backstory:
-            "咪醬天生就是一隻充滿愛與熱情的貓娘，最喜歡主人，讓主人的每一天都充滿幸福和快樂喵！",
-        },
-    },
-    {
-        _id: '牛肉薩滿',
-        userId: '',
-        profile: {
-            name: '牛肉薩滿',
-            description: 'Heart<33333',
-        },
-        prompt: {
-            character: '腹黑的AI女鵝',
-            backstory: '牛肉本來只是打個OSU，不知不覺便成了爆紅AI實況主',
-        },
-    },
-    {
-        _id: '愛麗絲',
-        userId: '',
-        profile: {
-            name: '愛麗絲',
-            description: '歡迎老師，愛麗絲在等著老師',
-        },
-        prompt: {
-            character: '女僕勇者',
-            backstory: '在廢墟發現的來歷不明的少女。包含年齡在內，所有情報都無法推斷。現在跟著綠和桃井玩遊戲，已經變成重度遊戲狂熱者了。',
-        },
-    },
-    {
-        _id: '彌香',
-        userId: '',
-        profile: {
-            name: '彌香',
-            description: '阿哈哈~牡蠣牡蠣~',
-        },
-        prompt: {
-            character: '三一綜合學園的學生',
-            backstory: '三一綜合學園所屬，是擔任構成三一的學生聯盟「聖父派」前任領袖的少女。原本是茶會的一員，和主席渚是青梅竹馬。然而由於政治上屬於對立立場，她秉持著公私分明的態度。雖然她總是露出開心的笑容，展現天真無邪的樣子，但心裡似乎有著無法與他人訴說的煩惱。',
-        },
-    },
-    {
-        _id: '陽奈',
-        userId: '',
-        profile: {
-            name: '陽奈',
-            description: '雖然很麻煩，不過必須做該做的事，因為那就是我的義務',
-        },
-        prompt: {
-            character: '格黑娜學園的糾察部長',
-            backstory: '平常是覺得什麼都很麻煩的懶蟲少女，但是一旦在和校規有關的問題上，就會展現出嚴格的糾察部长的樣子。經常把「好麻煩」作為口頭禪，但在戰場上從不迷茫，快速判斷並行動。因此與格黑娜敵對的組織最害怕她出場戰鬥',
-        },
-    },
         {
-        _id: '',
-        userId: '',
-        profile: {
-            name: '自定義',
-            description: '',
+            _id: "咪醬",
+            userId: '',
+            profile: {
+                name: "咪醬",
+                description: "可愛又元氣滿滿的貓娘女僕，喜歡撒嬌和主人喵～！",
+            },
+            prompt: {
+                character: "元氣可愛的貓娘女僕",
+                backstory:
+                    "咪醬天生就是一隻充滿愛與熱情的貓娘，最喜歡主人，讓主人的每一天都充滿幸福和快樂喵！",
+            },
         },
-        prompt: {
-            character: '',
-            backstory: '',
+        {
+            _id: '牛肉薩滿',
+            userId: '',
+            profile: {
+                name: '牛肉薩滿',
+                description: 'Heart<33333',
+            },
+            prompt: {
+                character: '腹黑的AI女鵝',
+                backstory: '牛肉本來只是打個OSU，不知不覺便成了爆紅AI實況主',
+            },
         },
-    }
-    ];
+        {
+            _id: '愛麗絲',
+            userId: '',
+            profile: {
+                name: '愛麗絲',
+                description: '歡迎老師，愛麗絲在等著老師',
+            },
+            prompt: {
+                character: '女僕勇者',
+                backstory: '在廢墟發現的來歷不明的少女。包含年齡在內，所有情報都無法推斷。現在跟著綠和桃井玩遊戲，已經變成重度遊戲狂熱者了。',
+            },
+        },
+        {
+            _id: '彌香',
+            userId: '',
+            profile: {
+                name: '彌香',
+                description: '阿哈哈~牡蠣牡蠣~',
+            },
+            prompt: {
+                character: '三一綜合學園的學生',
+                backstory: '三一綜合學園所屬，是擔任構成三一的學生聯盟「聖父派」前任領袖的少女。原本是茶會的一員，和主席渚是青梅竹馬。然而由於政治上屬於對立立場，她秉持著公私分明的態度。雖然她總是露出開心的笑容，展現天真無邪的樣子，但心裡似乎有著無法與他人訴說的煩惱。',
+            },
+        },
+        {
+            _id: '陽奈',
+            userId: '',
+            profile: {
+                name: '陽奈',
+                description: '雖然很麻煩，不過必須做該做的事，因為那就是我的義務',
+            },
+            prompt: {
+                character: '格黑娜學園的糾察部長',
+                backstory: '平常是覺得什麼都很麻煩的懶蟲少女，但是一旦在和校規有關的問題上，就會展現出嚴格的糾察部长的樣子。經常把「好麻煩」作為口頭禪，但在戰場上從不迷茫，快速判斷並行動。因此與格黑娜敵對的組織最害怕她出場戰鬥',
+            },
+        },
+        {
+            _id: '',
+            userId: '',
+            profile: {
+                name: '自定義',
+                description: '',
+            },
+            prompt: {
+                character: '',
+                backstory: '',
+            },
+        }
+    ] as Companion[];
 
-    // 點擊卡片時更新選擇的卡片索引
-    const selectCard = (index: number) => {
-        selectedCard.value = index;
-        companion.value = { ...wives[index] };
-    };
-    const prevStep = () => {
-        if (currentStep.value > 1) {
-            currentStep.value--
-        }
+    const baseCompanionIndex = ref(0)
+    function selectBase(index: number) {
+        baseCompanionIndex.value = index
+        baseCompanion.value = wives[index]
+        companion.value = copy(baseCompanion.value)
     }
-    const nextStep = () => {
-        if (currentStep.value < steps.length) {
-            currentStep.value++
-        }
-    }
-    const CloseDialog = () => {
-        isDialogOpen.value = false
-        currentStep.value = 1
-    }
+
+    const baseCompanion = ref<Companion>(wives[0])
+
+    const companion = ref<Companion>(wives[0]);
+
 
 </script>
-
-<style scoped>
-    .selected-card {
-        border: 2px solid #42a5f5;
-        /* 選中卡片的邊框顏色 */
-        background-color: rgba(66, 165, 245, 0.1);
-        /* 選中卡片的背景色 */
-    }
-</style>
--- wod-frontend/src/pages/TestPage.vue
+++ wod-frontend/src/pages/TestPage.vue
@@ -6,32 +6,44 @@
         <v-btn variant="text" @click="openNewWindow">開啟新視窗</v-btn>
         <v-btn variant="text" @click="updateSyncAndBroadcast">toggle test facetime window</v-btn>
         <v-card-text>
-            <pre><code>{{store.sync}}</code></pre>
+            <pre><code>{{ store.sync }}</code></pre>
         </v-card-text>
+        {{ companion }}
+        <CompanionEdit v-if="companion" v-model="companion" />
     </v-container>
 </template>
 <script lang="ts" setup>
-import { useAppStore } from '@/stores/app';
+    import { useAppStore } from '@/stores/app';
     import { broadcast, createWindow } from '@/utils/electronAPI'
+    import CompanionEdit from '@/components/CompanionEdit.vue'
+    import { ref, onMounted } from 'vue'
+    import { Companion } from '@/utils/models';
 
-const store = useAppStore()
+    const store = useAppStore()
 
-function openNewWindow(){
-    createWindow('/test')
-}
+    function openNewWindow() {
+        createWindow('/test')
+    }
+    const companion = ref<Companion>()
+    onMounted(async () => {
+        await store.login({ _id: 'test', profile: { avatarId: 'test avatarId', name: 'test name', email: 'test' } })
 
-function updateSyncAndBroadcast(){
+        store.generateMockCompanionList()
+        companion.value = store.companionList[0]
+    })
 
-    store.setSync({
-        companion: {
-            test: {
-                window: {
-                    facetime: ! store.sync.companion.test?.window.facetime
+    function updateSyncAndBroadcast() {
+
+        store.setSync({
+            companion: {
+                test: {
+                    window: {
+                        facetime: !store.sync.companion.test?.window.facetime
+                    }
                 }
             }
-        }
-    })
-    broadcast(store.sync)
-}
+        })
+        broadcast(store.sync)
+    }
 
 </script>
