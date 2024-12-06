fcdf941e776f5c55f4369f16cfd7262e77a66930
Kulimi <kulimi@kulimi.tw>
2024-12-06 23:44:10 +0800

    fix(CompanionEdit): 修復在載入時沒有檢查是否載入完畢，並且添加更安全的同步方法

--- wod-frontend/src/components/CompanionEdit.vue
+++ wod-frontend/src/components/CompanionEdit.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-card class="mx-auto" style="max-width: 500px">
-        <div class="d-flex" >
+    <v-card class="mx-auto" style="max-width: 500px" v-if="companion">
+        <div class="d-flex">
             <div class="pa-4 pt-10 text-center">
                 <label style="font-size: 14px; display: block; margin-bottom: 10px;">avatar here </label>
                 <!-- Custom upload button -->
@@ -28,50 +28,57 @@
                             box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
                             transition: transform 0.2s ease, box-shadow 0.2s ease;
                             z-index: 2;
-                            " @mouseover="handleMouseOver"
-                            @mouseleave="handleMouseLeave">
+                            " @mouseover="handleMouseOver" @mouseleave="handleMouseLeave">
                             上傳模型
                         </button>
-                    </div>   
+                    </div>
                 </div>
             </div>
             <div>
                 <v-form ref="form" v-model="isValid" class="pa-4 pt-10">
-                <v-textarea v-model="companion.profile.name" label="姓名" :rules="[rules.name]"
-                    rows="1" variant="filled" auto-grow></v-textarea>
-                <v-textarea v-model="companion.profile.description" label="描述" :rules="[rules.description]"
-                rows="1" variant="filled" auto-grow></v-textarea>
-                <v-textarea v-model="companion.prompt.character" label="角色" :rules="[rules.character]"
-                    rows="1" variant="filled" auto-grow></v-textarea>
-                <v-textarea v-model="companion.prompt.backstory"  label="背景故事" :rules="[rules.backStory]"
-                    rows="1" variant="filled" auto-grow></v-textarea>
-            </v-form>
+                    <v-textarea v-model="companion.profile.name" label="姓名" :rules="[rules.name]" rows="1"
+                        variant="filled" auto-grow></v-textarea>
+                    <v-textarea v-model="companion.profile.description" label="描述" :rules="[rules.description]" rows="1"
+                        variant="filled" auto-grow></v-textarea>
+                    <v-textarea v-model="companion.prompt.character" label="角色" :rules="[rules.character]" rows="1"
+                        variant="filled" auto-grow></v-textarea>
+                    <v-textarea v-model="companion.prompt.backstory" label="背景故事" :rules="[rules.backStory]" rows="1"
+                        variant="filled" auto-grow></v-textarea>
+                </v-form>
             </div>
         </div>
-     
-     
+
+
     </v-card>
 </template>
 <script lang="ts" setup>
     import { Companion } from '@/utils/model';
-    import { PropType, ref, watch } from 'vue';
+    import { ref, watch, PropType, onMounted } from 'vue';
+    import { copy } from '@/utils/utils'
 
-    
+
     const isValid = ref(false)
-    
+
     const props = defineProps({
         modelValue: {
-            type: Object as () => Companion,
-                required: true
-            }
-        })
-    
+            type: Object as PropType<Companion>,
+            required: true
+        }
+    })
+
     const emits = defineEmits(['update:modelValue'])
 
-    const companion = ref<Companion>({ ...props.modelValue });
+    const companion = ref<Companion>();
+    function loadProp() {
 
-    watch(companion, () => {
-        emits('update:modelValue', companion.value);
+        companion.value = copy(props.modelValue)
+    }
+    onMounted(() => {
+        loadProp()
+    })
+    watch(() => JSON.stringify(props.modelValue), loadProp)
+    watch(() => JSON.stringify(companion.value), () => {
+        emits('update:modelValue', copy(companion.value));
     });
 
     const rules = {
@@ -82,7 +89,7 @@
     };
 
     // Handle user-uploaded
-    const handleFileUpload = (event : Event) => {
+    const handleFileUpload = (event: Event) => {
         // 確保 event.target 是 HTMLInputElement
         const input = event.target as HTMLInputElement;
         const file = input.files?.[0]; // 使用可選鏈接
@@ -93,17 +100,17 @@
     };
 
     const handleMouseOver = (event: MouseEvent) => {
-    const target = event.target as HTMLButtonElement;
-    if (target) {
-        target.style.transform = 'scale(1.05)';
-    }
+        const target = event.target as HTMLButtonElement;
+        if (target) {
+            target.style.transform = 'scale(1.05)';
+        }
     };
 
     const handleMouseLeave = (event: MouseEvent) => {
-    const target = event.target as HTMLButtonElement;
-    if (target) {
-        target.style.transform = 'scale(1)';
-    }
+        const target = event.target as HTMLButtonElement;
+        if (target) {
+            target.style.transform = 'scale(1)';
+        }
     };
 
 </script>
