4692594ce60426271da76d81601aa7b6ed0c05a9
Kulimi <kulimi@kulimi.tw>
2024-12-30 21:07:04 +0800

    Fix live2d size and multi use (#9)
    * feat: CreateNewWife didn't show full CompanionPreview
    
    * fix: fix live2d size

--- wod-frontend/src/components/Live2dComponent.vue
+++ wod-frontend/src/components/Live2dComponent.vue
@@ -1,41 +1,38 @@
 <template>
-  <canvas
-    id="canvas_view"
-    style="
-      display: block;
-      margin: 0 auto;
-      position: relative;
-      z-index: 1;
-      width: 100%;
-      height: 100%;
-    "
-  ></canvas>
+  <div style="width: 100%;
+    height: 100%;
+    max-width: 100%;
+    max-height: 100%;" class="overflow-hidden">
+
+    <canvas v-if="elementId" :id="elementId" style="
+    display: block;
+    margin: 0 auto;
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    max-width: 100%;
+    max-height: 100%;
+    "></canvas>
+  </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue';
-import { init } from '../utils/live2d'; // Import your Live2D setup logic
+<script setup lang="ts">
+  import { ref, onMounted, nextTick } from 'vue';
+  import { init } from '../utils/live2d'; // Import your Live2D setup logic
 
-// State for error handling
-const isModelLoaded = ref(false);
+  const elementId = ref<string>(crypto.randomUUID() + "_live2d");
+  onMounted(async () => {
+    console.log('Live2DComponent: Mounted hook started...');
 
-onMounted(async () => {
-  console.log('Live2DComponent: Mounted hook started...');
-  try {
-    await init();
-    isModelLoaded.value = true;
-    console.log('Live2DComponent: Live2D model initialized successfully.');
-  } catch (error) {
-    isModelLoaded.value = false;
-    console.error('Live2DComponent: Failed to initialize Live2D model:', error);
-  }
-});
+    nextTick(async () => {
+
+      try {
+        await init(elementId.value);
+        console.log('Live2DComponent: Live2D model initialized successfully.');
+      } catch (error) {
+        console.error('Live2DComponent: Failed to initialize Live2D model:', error);
+      }
+    })
+  });
 </script>
-
-<style scoped>
-/* Add any specific styling for the canvas if needed */
-canvas {
-  width: 100%;
-  height: 100%;
-}
-</style>
